c9d970c1cadabdcac222b72c22ff63c9ab89d1aa
mattndross <mattndross@users.noreply.github.com>
2021-09-27 13:27:37 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="container">
+    <div className="sidebar">
+
+    </div>
+
+    <div className="main">
+    <h1>Buttons</h1>
+    <Button />
+    <Button variant="outline" />
+    </div>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button(props) {
+    return (
+        <div>
+            <button className="props.variant" >Default</button>
+        </div>
+    )
+}
